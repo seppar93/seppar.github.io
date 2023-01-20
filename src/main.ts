2bdda1f9372e82912834702ce7ee72a26464a5c4
Sepehr Parirokh <sepehrparirokh@gmail.com>
2023-01-20 17:27:16 -0500

    bug fix and animation

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import * as THREE from "three";
 // 1. Scene => container holding objects
 const scene = new THREE.Scene();
 // 2. Camera => looking inside of Scene (eyeballs)
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 //                                        ^^ field of viw,  ^^^aspect ration, view frustum
 // 3. Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -35,3 +30,16 @@ const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
 const torus = new THREE.Mesh(geometry, material)
 
 scene.add(torus)
+
+// 
+function animate () {
+  // telling the browesr to perform an animation
+  // game loop
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
